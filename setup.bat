03ade94a0c60d6a63a0a6baa1440922946ff85ee
Gao2546 <athipyuthaworawit@gmail.com>
2024-01-27 22:16:57 +0700

    add_new_requirement

--- setup.bat
+++ setup.bat
@@ -2,5 +2,5 @@ mkdir pyenv
 cd pyenv
 python -m venv pytorch
 cd ..
-"./pyenv/pytorch/Scripts/pip" install --upgrade pip
+"./pyenv/pytorch/Scripts/python" -m pip install --upgrade pip
 "./pyenv/pytorch/Scripts/pip" install -r requirements.txt
