547648e44991b9418201e2404a45593bb2c96f0c
Gao2546 <athipyuthaworawit@gmail.com>
2024-02-04 17:19:27 +0700

    add gitignore and pull_open_vs_code.bat

--- pull_open_vs_code.bat
+++ pull_open_vs_code.bat
@@ -1,6 +1,6 @@
 set PATH=%USERPROFILE%\AppData\Local\Microsoft\WindowsApps;C:\Program Files\Microsoft VS Code\bin;%PATH%
 "./pyenv/pytorch/Scripts/python" -m pip install --upgrade pip
 "./pyenv/pytorch/Scripts/python" -m pip install -r requirements.txt
-cd ./debug/web_scraping
+cd ./debug/model_text_to_text(code)
 git pull
 code .
